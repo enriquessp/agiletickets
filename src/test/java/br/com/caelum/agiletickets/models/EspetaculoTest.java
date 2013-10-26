858868c607fa602869e6a1340144d4d8042d67cc
Matheus <Matheus>
2013-10-26 16:46:52 -0200

    TDD Sessao Semanal

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -115,9 +115,37 @@ public class EspetaculoTest {
 		LocalDate fim = inicio.minusDays(1);
 		LocalTime horario = new LocalTime(19,0);
 		
-		List<Sessao> sessoes = show.criaSessoes(inicio, fim, horario, Periodicidade.DIARIA);
+		show.criaSessoes(inicio, fim, horario, Periodicidade.DIARIA);
 		
 	}
+	
+//	@Test
+//	public void DeveCriarUmaSessaoSemanalSeInicioEFimMenorQueUmaSemana() {
+//		Espetaculo show = new Espetaculo();
+//		LocalDate inicio = new LocalDate(2013,10,20);
+//		LocalDate fim = inicio.plusDays(1);
+//		LocalTime horario = new LocalTime(19,0);
+//		
+//		List<Sessao> sessoes = show.criaSessoes(inicio, fim, horario, Periodicidade.SEMANAL);
+//		
+//		assertEquals(1, sessoes.size());
+//		assertEquals(inicio.toDateTime(horario), sessoes.get(0).getInicio());
+//		assertEquals(fim.toDateTime(horario), sessoes.get(1).getInicio());		
+//	}
+//	
+//	@Test
+//	public void DeveCriarDuasSessoesSemanaisSeInicioEFimMaiorQueUmaSemana(){
+//		Espetaculo show = new Espetaculo();
+//		LocalDate inicio = new LocalDate(2013,10,20);
+//		LocalDate fim = inicio.plusDays(7);
+//		LocalTime horario = new LocalTime(19,0);
+//		
+//		List<Sessao> sessoes = show.criaSessoes(inicio, fim, horario, Periodicidade.SEMANAL);
+//		
+//		assertEquals(2, sessoes.size());
+//		assertEquals(inicio.toDateTime(horario), sessoes.get(0).getInicio());
+//		assertEquals(fim.toDateTime(horario), sessoes.get(1).getInicio());				
+//	}
 
 	private Sessao sessaoComIngressosSobrando(int quantidade) {
 		Sessao sessao = new Sessao();
